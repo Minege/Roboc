a70a59dea3764a63501fb9eb60842c58d58ec2a5
Loan <minege02@gmail.com>
2015-10-31 00:15:38 +0100

    Problème de boucle au niveau de l'affichage: KeyError: (1, 11)

--- Labyrinthe.py
+++ Labyrinthe.py
@@ -12,25 +12,40 @@ class Labyrinthe:
         # Initialisation du dictionnaire
         ligne = 0
         colonne = -1
+        colonne_liste = []
         for lettre in obstacles:
             if lettre == "\n":
+                colonne_liste.append([ligne, colonne])
                 ligne += 1
-                colonne = 0
+                colonne = -1
             colonne += 1
             self.grille[ligne, colonne] = lettre
         # Affichage POUR QUE CA MARCHE IL FAUT QUE SELF.GRILLE AIS PAS DE LA MERDE DEDANS
         l = 0
         c = 0
         ligne_str = ""
-        while l < ligne:
-            while c < colonne:
-                ligne_str += self.grille[ligne, colonne]
-                c += 1
-            l += 1
-            c = 0
-            print(ligne_str, end="")
-            ligne_str = ""  # Clean de la ligne, pour afficher les autres
-
+        i = 0
+        # while i < len(colonne_liste):
+        #    while l < colonne_liste[i][0]:
+        #        while c < colonne_liste[i][1]:
+        #            ligne_str += self.grille[l, c]
+        #            c += 1
+        #        l += 1
+        #        c = 0
+        #        print(ligne_str, end="")
+        #        ligne_str = ""  # Clean de la ligne, pour afficher les autres
+        #    i += 1
+        print(colonne_liste)
+        l = 0
+        c = 0
+        nb = 0
+        while nb < len(colonne_liste):
+            while l < colonne_liste[nb][0]:
+                while c < colonne_liste[nb][1]:
+                    print(self.grille[l, c], end="")
+                    c += 1
+                l += 1
+                nb += 1
             # print("AFFICHAGE DE LA GRILLE avec le string directe:")
             # la = 0
             # ca = 0
