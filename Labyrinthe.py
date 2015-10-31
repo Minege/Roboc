4ce3a35eeed014d1ce61b127cabd6125faa59458
Loan <minege02@gmail.com>
2015-10-31 22:08:45 +0100

    On utilise une liste plutôt que un dico

--- Labyrinthe.py
+++ Labyrinthe.py
@@ -8,53 +8,28 @@ class Labyrinthe:
 
     def __init__(self, robot, obstacles):
         self.robot = robot
-        self.grille = {}
+        self.grille = []
         # Initialisation du dictionnaire
-        ligne = 0
-        colonne = -1
-        colonne_liste = []
+        nb_de_char_grille = []
+        char_grille = []
+        nb_de_char = 0
         for lettre in obstacles:
+            nb_de_char += 1
             if lettre == "\n":
-                colonne_liste.append([ligne, colonne])
-                ligne += 1
-                colonne = -1
-            colonne += 1
-            self.grille[ligne, colonne] = lettre
-        # Affichage POUR QUE CA MARCHE IL FAUT QUE SELF.GRILLE AIS PAS DE LA MERDE DEDANS
-        l = 0
-        c = 0
-        ligne_str = ""
-        i = 0
-        # while i < len(colonne_liste):
-        #    while l < colonne_liste[i][0]:
-        #        while c < colonne_liste[i][1]:
-        #            ligne_str += self.grille[l, c]
-        #            c += 1
-        #        l += 1
-        #        c = 0
-        #        print(ligne_str, end="")
-        #        ligne_str = ""  # Clean de la ligne, pour afficher les autres
-        #    i += 1
-        print(colonne_liste)
-        l = 0
-        c = 0
-        nb = 0
-        while nb < len(colonne_liste):
-            while l < colonne_liste[nb][0]:
-                while c < colonne_liste[nb][1]:
-                    print(self.grille[l, c], end="")
-                    c += 1
-                l += 1
-                nb += 1
-            # print("AFFICHAGE DE LA GRILLE avec le string directe:")
-            # la = 0
-            # ca = 0
-            # ligne_str = ""
-            # for lettre in obstacles:
-            #    if lettre == "\n":
-            #        la += 1
-            #        ca = 0
-            #        print(ligne_str, end="")
-            #        ligne_str = ""
-            #    ligne_str += lettre
-            #    ca += 1
+                nb_de_char_grille.append(nb_de_char)
+                nb_de_char = 0
+            char_grille.append(lettre)
+
+        # On a la longueur de chaque ligne, et les caractères
+        i = 1
+        while i < len(nb_de_char_grille):
+            for numero_ligne in range(5):
+                ligne = [char_grille[i]] * nb_de_char_grille[i]  # Je crée une liste avec 6 "X" dedans
+                self.grille.append(ligne)
+                i += 1
+        print(self.grille)
+
+        for ligne in self.grille:
+            for case in ligne:
+                print(case, end="")
+            print()  # Après une ligne on fait un retour à la ligne
